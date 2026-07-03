59045636cdfc5e32391d2b0fc3ff6df8393f3fe3
Mendel <MendelCohen770@gmail.com>
2026-07-03 13:19:50 +0300

    add ReliabilityCalculator

--- Calculators/ReliabilityCalculator.cs
+++ Calculators/ReliabilityCalculator.cs
@@ -1,4 +1,26 @@
+using IntelligencePipeline.Models.Reports;
+
 namespace IntelligencePipeline.Calculators
 {
+
+    class ReliabilityCalculator
+    {
+        const int MIN_RELIABILITY_SCORE = 1;
+        const int MAX_RELIABILITY_SCORE = 10;
+
+        /// <summary>
+        /// Calls report.CalculateReliabilityScore() and ensures result is 1–10
+        /// </summary>
+        /// <param name="report"> a Report instance of any type </param>
+        /// <returns> int Reliability Score clamps to 1-10</returns>
+        public int Calculate(Report report)
+        {
+            int reliabilityScore = report.CalculateReliabilityScore();
+
+            int reliabilityScoreClamp = Math.Clamp(reliabilityScore, MIN_RELIABILITY_SCORE, MAX_RELIABILITY_SCORE);
+
+            return reliabilityScoreClamp;
+        }
+    }
 
 }
