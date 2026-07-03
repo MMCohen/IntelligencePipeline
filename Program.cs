4a1f92e68e914957937c1b50a651d40239e08e06
Mendel <MendelCohen770@gmail.com>
2026-07-03 10:23:52 +0300

    add RadarValidator

--- Program.cs
+++ Program.cs
@@ -14,10 +14,23 @@ namespace IntelligencePipeline
             SoldierReport sr = new SoldierReport(13, DateTime.Parse("01/01/2020"), 30, 35,
                 "hello world how", "Moshe", "1234567", "99", 4);
 
-            SoldierValidator x = new SoldierValidator();
-            ValidationResult y = x.Validate(sr);
-            Console.WriteLine(y.ErrorMessage);
+            //SoldierValidator x = new SoldierValidator();
+            //ValidationResult y = x.Validate(sr);
+            //Console.WriteLine(y.ErrorMessage);
             //Console.WriteLine(x.Validate(rp));
+
+            // radar testing
+            RadarReport radarReport = new RadarReport(12, DateTime.Parse("01/01/2020"), 30, 35, "hello world how",
+                2000, 359, 200);
+
+            RadarValidator radarValidator = new RadarValidator();
+            ValidationResult radarResult = radarValidator.Validate(radarReport);
+
+            Console.WriteLine(radarResult.ErrorMessage);
+
+            //
+
+
 
 
         }
--- Validation/RadarValidator.cs
+++ Validation/RadarValidator.cs
@@ -1,7 +1,27 @@
-//namespace IntelligencePipeline.Validation
-//{
-//    class RadarValidator : BaseValidator, IValidator
-//    {
+using IntelligencePipeline.Models.Reports;
 
-//    }
-//}
+namespace IntelligencePipeline.Validation
+{
+    class RadarValidator : BaseValidator, IValidator
+    {
+
+        protected override ValidationResult ValidateSpecificFields(Report report)
+        {
+            if (report is not RadarReport radarReport)
+                return ValidationResult.Failure("report is not Radar report");
+
+            if (radarReport.Speed < 0 || radarReport.Speed > 2000)
+                return ValidationResult.Failure("speed must be between 0-2,000");
+
+            if (radarReport.Direction < 0 || radarReport.Direction > 359)
+                return ValidationResult.Failure("Direction must be between 0-359");
+
+            if (radarReport.Distance < 100 || radarReport.Distance > 100000)
+                return ValidationResult.Failure("Distance must be between 100-100,000");
+
+            return ValidationResult.Success();
+        }
+
+
+    }
+}
